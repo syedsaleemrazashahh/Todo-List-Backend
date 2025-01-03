db70ca557e57ecadfae984c9a41948088f925afa
Syed Saleem Raza Shah <syedsaleemrazashah69@gmail.com>
2025-01-03 20:51:10 +0500

    30% working compelete

--- server.js
+++ server.js
@@ -1,21 +1,98 @@
-import express, { response } from "express";
+import express from "express";
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
+const todos = [];
+
+app.use(express.json()); // ye body ko JSON mein convert karta hai
+
+// Get all todos
 app.get("/get-all-todos", (request, response) => {
-  response.send("<h1>Syed Saleem Raza Shah</h1>");
+  const message = !todos.length ? "todos empty" : "ye lo sab todos";
+
+  response.send({ data: todos, message: message });
 });
 
-// ye api new todo banati hai
-app.post("/add-todo", (request, response) => {});
+// naya todo banane ki api hai
+app.post("/add-todo", (request, response) => {
+  if (!request.body.todo) {
+    return response.status(400).send({ message: "todo content required" });
+  }
 
-// ye todo ko edit karne ki api hai
-app.patch("/edit-todo/:id", (request, response) => {});
+  const object = {
+    todoContent: request.body.todo,
+    id: String(new Date().getTime()),
+  };
+
+  todos.push(object);
+
+  response.send({ message: "add ho raha hai", data: object });
+});
+
+// todo ko edit karne ki api hai
+app.patch("/edit-todo/:id", (request, response) => {
+  const id = request.params.id;
+
+  let isFound = false;
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id === id) {
+      // idher product mil chuka hy (ab us product ko edit karna hy)
+
+      todos[i].todoContent = request.body.todoContent;
+      isFound = true;
+      break;
+    }
+  }
+  if (isFound) {
+    response.status(201).send({
+      data: {
+        todoContent: request.body.todoContent,
+        id: id,
+      },
+      message: "todo updated ho gaya hai!",
+    });
+  } else {
+    response.status(200).send({
+      data: null,
+      message: "todo not found",
+    });
+  }
+});
+
+// todo ko delete karne ki api hai
+app.delete("/delete-todo/:id", (request, response) => {
+  const id = request.params.id;
+
+  let isFound = false;
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id === id) {
+      // idher product mil chuka hy (ab us product ko deleted karna hy)
+      todos.splice(i, 1);
+      isFound = true;
+      break;
+    }
+  }
+  if (isFound) {
+    response.status(201).send({
+      data: {
+        todoContent: request.body.todoContent,
+        id: id,
+      },
+      message: "todo deleted ho gaya hai!",
+    });
+  } else {
+    response.status(200).send({
+      data: null,
+      message: "todo not found",
+    });
+  }
+});
 
 app.use((request, response) => {
   response.status(404).send("No Route Found!");
 });
 
+// Start server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
